9fb51c7f77590378e7d38a11ccc0754c93ae88aa
Jerome Dane <jerome.dane@gmail.com>
2015-10-14 16:46:41 -0400

    Wrap everything with a class

--- includes/userscript.user.js
+++ includes/userscript.user.js
@@ -32,107 +32,112 @@
 // CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
 // OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// configuration variables
-var feedItemSelector = '#browse-items-primary .feed-item-container';
-
-function simulateClick(element) {
+function RemoveFeedFromYouTube() {
 	
-    var clickEvent;
-    clickEvent = document.createEvent("MouseEvents");
-    clickEvent.initEvent("mousedown", true, true);
-    element.dispatchEvent(clickEvent);
-    
-    clickEvent = document.createEvent("MouseEvents");
-    clickEvent.initEvent("click", true, true);
-    element.dispatchEvent(clickEvent);
-    
-    clickEvent = document.createEvent("MouseEvents");
-    clickEvent.initEvent("mouseup", true, true);
-    element.dispatchEvent(clickEvent);
-}
+	// configuration variables
+	var feedItemSelector = '#browse-items-primary .feed-item-container';
 
-// inject styles
-function injectStyle() {
-	var style = document.createElement('style');
-	style.type = 'text/css';
-	style.innerHTML = '.feed-item-dismissal-notices { display:none; }' + // hide removed from feed notices 
-		'.bcRemoveButton {' +
-			'cursor:pointer;' +
-			'opacity:.45;' +
-			'position:absolute; top:0; right:30px; display:none; padding:3px' +
-		'}' +
-		'.feed-item-container:hover .bcRemoveButton { display:block; }' +
-		'.bcRemoveButton:hover { opacity:.6; }';
-	document.getElementsByTagName('head')[0].appendChild(style);
-}
+	function simulateClick(element) {
 
-function getRemoveTrigger(postElem) {
-	var removeTrigger = postElem.querySelector('.dismiss-menu-choice');
-	return removeTrigger;
-}
+		var clickEvent;
+		clickEvent = document.createEvent("MouseEvents");
+		clickEvent.initEvent("mousedown", true, true);
+		element.dispatchEvent(clickEvent);
 
-function removePost(postElem) {
-	var removeTrigger = getRemoveTrigger(postElem);
-	simulateClick(removeTrigger);
-	postElem.remove();
-	// hack to auto load images for vids as they come into view
-	var x = window.scrollX;
-	var y = window.scrollY;
-	window.scroll(x,y+1);
-	setTimeout(function() {
-		window.scroll(x,y);
-	},50);
-}
+		clickEvent = document.createEvent("MouseEvents");
+		clickEvent.initEvent("click", true, true);
+		element.dispatchEvent(clickEvent);
 
-// create a new button element in the document
-function createNewButtonElement() {
-	var src = chrome.extension.getURL('images/close_16_r8.png');
-	var button = document.createElement('img');
-	button.src = src;
-	button.className = 'bcRemoveButton';
-	button.title = "Remove this item from your subscription feed";
-	return button;
-}
+		clickEvent = document.createEvent("MouseEvents");
+		clickEvent.initEvent("mouseup", true, true);
+		element.dispatchEvent(clickEvent);
+	}
 
-// inject a remove button into a post
-function injectButton(postElem) {
-	if(!postElem.className.match(/buttonEnabled/)) {
-		postElem.className += ' buttonEnabled';
+	// inject styles
+	function injectStyle() {
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		style.innerHTML = '.feed-item-dismissal-notices { display:none; }' + // hide removed from feed notices 
+			'.bcRemoveButton {' +
+				'cursor:pointer;' +
+				'opacity:.45;' +
+				'position:absolute; top:0; right:30px; display:none; padding:3px' +
+			'}' +
+			'.feed-item-container:hover .bcRemoveButton { display:block; }' +
+			'.bcRemoveButton:hover { opacity:.6; }';
+		document.getElementsByTagName('head')[0].appendChild(style);
+	}
+
+	function getRemoveTrigger(postElem) {
+		var removeTrigger = postElem.querySelector('.dismiss-menu-choice');
+		return removeTrigger;
+	}
+
+	function removePost(postElem) {
 		var removeTrigger = getRemoveTrigger(postElem);
-		if(removeTrigger) {
-			var actionMenuElem = postElem.querySelector('.feed-item-action-menu');
-			var button = createNewButtonElement();
-			actionMenuElem.parentNode.insertBefore(button, actionMenuElem);
-			button.onclick = function() {
-				removePost(postElem);
-			};
+		simulateClick(removeTrigger);
+		postElem.remove();
+		// hack to auto load images for vids as they come into view
+		var x = window.scrollX;
+		var y = window.scrollY;
+		window.scroll(x,y+1);
+		setTimeout(function() {
+			window.scroll(x,y);
+		},50);
+	}
+
+	// create a new button element in the document
+	function createNewButtonElement() {
+		var src = chrome.extension.getURL('images/close_16_r8.png');
+		var button = document.createElement('img');
+		button.src = src;
+		button.className = 'bcRemoveButton';
+		button.title = "Remove this item from your subscription feed";
+		return button;
+	}
+
+	// inject a remove button into a post
+	function injectButton(postElem) {
+		if(!postElem.className.match(/buttonEnabled/)) {
+			postElem.className += ' buttonEnabled';
+			var removeTrigger = getRemoveTrigger(postElem);
+			if(removeTrigger) {
+				var actionMenuElem = postElem.querySelector('.feed-item-action-menu');
+				var button = createNewButtonElement();
+				actionMenuElem.parentNode.insertBefore(button, actionMenuElem);
+				button.onclick = function() {
+					removePost(postElem);
+				};
+			}
+		}
+	};
+
+	// draw mute button for existing posts
+	function injectButtonsIntoPosts() {
+		var videoElements = document.querySelectorAll(feedItemSelector);
+		for(var i = 0; i < videoElements.length; i++) {
+			injectButton(videoElements[i]);
 		}
 	}
-};
 
-// draw mute button for existing posts
-function injectButtonsIntoPosts() {
-	var videoElements = document.querySelectorAll(feedItemSelector);
-	for(var i = 0; i < videoElements.length; i++) {
-		injectButton(videoElements[i]);
-	}
+	injectStyle();
+	injectButtonsIntoPosts();
+
+	// trigger re-checking all posts when any new post is first moused over 
+	$(feedItemSelector).live('mouseover', function() {
+		injectButtonsIntoPosts();
+	});
+
+	// remove all watched button
+	$('.feed-header .feed-manage-link').after('<a id="bcRemoveAll" class="yt-uix-button  feed-manage-link secondary-nav yt-uix-sessionlink yt-uix-button-epic-nav-item">Remove All Watched</a>');
+	$('#bcRemoveAll').click(function() {
+		$(feedItemSelector).each(function() {
+			var postElem = $(this);
+			if($('.feed-thumb-watched', postElem).size() > 0) {
+				removePost(postElem);
+			}
+		});
+	});
 }
 
-injectStyle();
-injectButtonsIntoPosts();
-
-// trigger re-checking all posts when any new post is first moused over 
-$(feedItemSelector).live('mouseover', function() {
-	injectButtonsIntoPosts();
-});
-
-// remove all watched button
-$('.feed-header .feed-manage-link').after('<a id="bcRemoveAll" class="yt-uix-button  feed-manage-link secondary-nav yt-uix-sessionlink yt-uix-button-epic-nav-item">Remove All Watched</a>');
-$('#bcRemoveAll').click(function() {
-	$(feedItemSelector).each(function() {
-		var postElem = $(this);
-		if($('.feed-thumb-watched', postElem).size() > 0) {
-			removePost(postElem);
-		}
-	});
-});
+new RemoveFeedFromYouTube();
