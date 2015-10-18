07058b7dd77fc34194cf3de42037b666e00d626e
Jerome Dane <jerome.dane@gmail.com>
2015-10-18 18:45:38 -0400

    Updated user script head

--- src/userscript.head.js
+++ src/userscript.head.js
@@ -5,5 +5,12 @@
 // @author			<%= author %>
 // @include			http://www.youtube.com/feed/subscriptions*
 // @include			https://www.youtube.com/feed/subscriptions*
+// 
+// This project is hosted on GitHub at https://github.com/JeromeDane/YouTube-Remove-From-Feed-Button
+// 
+// Issue tracker: https://github.com/JeromeDane/YouTube-Remove-From-Feed-Button/issues
+// 
+// License: GPL-2.0
+// 
 // ==/UserScript==
 
