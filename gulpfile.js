5823720c57e5caf9aa3dd74540f35db1b251878e
Jerome Dane <jerome.dane@gmail.com>
2015-10-17 22:38:57 -0400

    User script build process

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,18 @@
  * watch-chrome		Create a full build for Chrome and automatically update it when files change
  */
 
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var closureCompiler = require('gulp-closure-compiler');
+var del = require('del');
 var fs = require("fs");
 var gulp = require('gulp');
 var gutil = require("gulp-util");
 var io = require('socket.io');
+var rename = require("gulp-rename");
 var template = require('gulp-template');
 var watch = require('gulp-watch');
-var webpack = require("webpack");
+var webpack = require('webpack-stream');
 var zip = require('gulp-zip');
 var ignore = require('gulp-ignore');
 var rimraf = require('gulp-rimraf');
@@ -25,7 +29,89 @@ function getPackageDetails() {
 
 // default gulp task
 gulp.task('default', ['dist-chrome'], function(callback) {
+	del("./build/userscript/userscript.body.js");
+	del("./build/userscript/userscript.head.js");
+	del("./build/userscript/userscript.min.js");
+});
 
+// create a minified userscript for final distribution
+gulp.task('userscript', ['script-merge-min'], function() {
+	del("build/userscript");
+});
+
+gulp.task('script-header', function() {
+	return gulp.src('./src/userscript/userscript.head.js')
+			.pipe(template(getPackageDetails()))
+			.pipe(gulp.dest('./build/userscript'));
+});
+
+gulp.task('script-min', ['script-merge'], function(callback) {
+	console.log("Minifying userscript ...");
+	/*
+	return gulp.src('dist/userscript/userscript.min.js')
+		.pipe(uglify())
+		.pipe(gulp.dest("./dist/userscript"));
+	*/
+	
+	
+	return gulp.src('dist/userscript/userscript.user.js')
+		.pipe(uglify())
+		.pipe(rename("userscript.min.js"))
+		.pipe(gulp.dest("./build/userscript"));
+/*
+			.pipe(closureCompiler({
+		  compilerPath: './node_modules/google-closure-compiler/compiler.jar',
+		  fileName: 'build/userscript/userscript.min.js',
+		  //continueWithWarnings: true
+		}), function() {
+			console.log("ERRORS =========");
+			callback();
+		});
+	*/
+});
+
+gulp.task('script-webpack', function(callback) {
+	// pack userscript
+	return gulp.src("./src/userscript.user.*")
+			.pipe(webpack({
+				module: {
+					loaders: [
+						{test: /\.png$/, loader: "url-loader?mimetype=image/png"},
+						{test: /\.css$/, loader: 'style!css' }
+					]
+				},
+				devtool: 'inline-source-map'
+			}))
+			.pipe(rename("userscript.body.js"))
+			.pipe(gulp.dest('./build/userscript'));
+});
+
+
+gulp.task('script-merge', ['script-webpack', 'script-header'], function(callback) {
+	return gulp.src(["./build/userscript/userscript.head.js", "./build/userscript/userscript.body.js"])
+			.pipe(concat('userscript.user.js'))
+			.pipe(gulp.dest("./dist/userscript"));
+});
+gulp.task('script-merge-min', ['script-min', 'script-header'], function(callback) {
+	console.log("Merging userscript header and minified code");
+	return gulp.src(["./build/userscript/userscript.head.js", "./build/userscript/userscript.min.js"])
+			.pipe(concat('userscript.user.min.js'))
+			.pipe(gulp.dest("./dist/userscript"));
+});
+
+gulp.task('script-build', ['script-merge'], function(callback) {
+	callback();
+});
+
+gulp.task('chrome-images', function() {
+	return gulp.src('./build/chrome/images/icon_*.*')
+			.pipe(gulp.dest('./build/chrome/temp/images'));
+});
+
+gulp.task('clean', function(callback) {
+	del('build');
+	del('dist');
+	callback();
 });
 
 // pre-copy and minify chrome distribution files before zipping them
@@ -49,45 +135,36 @@ gulp.task('dist-chrome-pre', ['build-chrome'], function(callback) {
 });
 
 // publish distribution for Chrome
-gulp.task('dist-chrome', ['dist-chrome-pre'], function(callback) {
+gulp.task('dist-chrome', ['build-chrome'], function(callback) {
+	
+	
+	// copy files to temporary directory
+	console.log('Copying chrome build to temporary ...');
+	gulp.src('./build/chrome/**').pipe(gulp.dest('./build/chrome/temp'));
+	
+	// copy images to temporary directory
+	//gulp.src('./build/chrome/images/icon_*.*').pipe(gulp.dest('./build/chrome/temp/images'));
+	
+	// minify userscript
+	gulp.src('build/chrome/*.js')
+		.pipe(closureCompiler({
+		  compilerPath: './node_modules/google-closure-compiler/compiler.jar',
+		  fileName: 'build/chrome/temp/userscript.user.js'
+		}));
+	
 	console.log('zipping chrome files');
 	// compress chrome build into a distribution zip
 	gulp.src('build/chrome/temp/**')
         .pipe(zip('chrome-extension-v' + getPackageDetails().version + '.zip'))
         .pipe(gulp.dest('dist'));
 
+	del('build/chrome/temp');
+
 	callback();
 });
 
-
 // build the full chrome distribution
-gulp.task('build-chrome', function(callback) {
-
-	// pack userscript
-	webpack({
-		entry: "./src/userscript.user.js",
-		output: {
-			path: "./build/chrome",
-			filename: "userscript.user.js"
-		},
-		module: {
-			loaders: [
-				{test: /\.png$/, loader: "url-loader?mimetype=image/png"},
-				{test: /\.css$/, loader: 'style!css' }
-			]
-		},
-		devtool: 'inline-source-map'
-	}, function(err, stats) {
-		if(err)
-			throw new gutil.PluginError("webpack", err);
-		gutil.log("[webpack]", stats.toString({
-			// output options
-		}));
-		callback();
-	});
-
-	// copy images
-	gulp.src('./src/images/icon_*.*').pipe(gulp.dest('./build/chrome/images'));
+gulp.task('build-chrome', ['script-build', 'chrome-images'], function(callback) {
 
 	// generate chrome manifest
 	gulp.src('./src/chrome/manifest.json')
@@ -98,34 +175,6 @@ gulp.task('build-chrome', function(callback) {
 	gulp.src('./src/chrome/options.html')
 			.pipe(template(getPackageDetails()))
 			.pipe(gulp.dest('./build/chrome'));
-
-});
-
-// build the full userscript distribution
-gulp.task('build-userscript', function(callback) {
-
-	// pack userscript
-	webpack({
-		entry: "./src/userscript.user.js",
-		output: {
-			path: "./build/userscript",
-			filename: "userscript.user.js",
-			library: "MyLibrary",
-			libraryTarget: "umd"
-		},
-		module: {
-			loaders: [
-				{test: /\.png$/, loader: "url-loader?mimetype=image/png"}
-			]
-		}
-	}, function(err, stats) {
-		if(err)
-			throw new gutil.PluginError("webpack", err);
-		gutil.log("[webpack]", stats.toString({
-			// output options
-		}));
-		callback();
-	});
 
 });
 
---
+++ src/userscript/userscript.head.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name			<%= fullName %>
+// @description		<%= description %>
+// @version			<%= version %>
+// @author			<%= author %>
+// @include			http://www.youtube.com/feed/subscriptions*
+// @include			https://www.youtube.com/feed/subscriptions*
+// ==/UserScript==
+
